e87a722dcd91e72bdb5f438843994529a3402dc9
Sadanand <sadanandsawargave@gmail.com>
2022-11-10 22:52:47 +0530

    Jenkins added

---
+++ src/main/java/com/SGS/JnDemo2Application.java
@@ -0,0 +1,13 @@
+package com.SGS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnDemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/SGS/JnDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SGS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
